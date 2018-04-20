b7373a41dd4480b943f25c44fc7dd2150c952159
GaoLei <gaolei@GaoLeideMacBook-Pro.local>
2018-04-20 08:28:28 +0800

    rename

---
+++ HelloIOS/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by gaolei on 2017/4/27.
+//  Copyright © 2017年 gao.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
