5b2e91d3a393860ed29c57e5fd28be8fff8ef611
gaolei <756985063@qq.com>
2021-11-22 21:42:28 +0800

    Refactor(Project): 重构Demo

---
+++ ios_app/Shared/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  Shared
+//
+//  Created by gaolei on 2021/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
